4b2727b4374e9120a8c7e9191ec3e35c47de3096
saiudja <elbaranova@gmail.com>
2024-10-22 12:24:05 +0300

    added links to the project

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ export default function App() {
       <h2>
         <Search />
       </h2>
+      <p>
+        This project was coded by Saiudja and is
+        <a href="https://github.com/saiudja/weather-react">
+          open-sourced on GitHub
+        </a>
+        and
+        <a href="https://hilarious-sopapillas-3f76f5.netlify.app/">
+          hosted on Netlify
+        </a>
+        <a></a>
+      </p>
     </div>
   );
 }
